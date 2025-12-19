a283cf2f87bafdf669faca77889ec7045f2e8e44
pepper <peppermaryparay@gmail.com>
2025-12-19 15:39:08 -0600

    renamed tsx to jsx and added different code.

---
+++ my-app/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+  <h1>Hello React!</h1>
